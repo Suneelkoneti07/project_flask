79b30438ff0aa06dea4ac7cd01e3f8eda987ec36
Suneelkoneti07 <163648927+Suneelkoneti07@users.noreply.github.com>
2025-07-31 21:11:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello from Flask CI/CD using Jenkins and Ansible!"
+    return "hello sunnel how are?"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
